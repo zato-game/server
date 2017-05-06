02b62fa815caf2d9792e875cc43209332bf9329f
Vilhelm Prytz <vpytkanal.sverige@gmail.com>
2017-05-06 20:43:04 +0200

    Add content from old git repo

---
+++ server.lua
@@ -0,0 +1,8 @@
+-- Server program for Zato
+
+-- static vars, added on a per node basis
+serverStatic = {}
+serverStatic.id = 0
+serverStatic.niceName = "Example"
+serverStatic.location = "EU/SE"
+serverStatic.api_key = ""
